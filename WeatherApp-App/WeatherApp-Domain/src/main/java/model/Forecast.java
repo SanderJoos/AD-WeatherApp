6d39afa4857ebccf7637a18e19f89206c22c5187
Sander Joos <sander.joos@skynet.be>
2016-09-22 16:08:01 +0200

    forecast filled in

--- WeatherApp-App/WeatherApp-Domain/src/main/java/model/Forecast.java
+++ WeatherApp-App/WeatherApp-Domain/src/main/java/model/Forecast.java
@@ -1,8 +1,47 @@
 package model;
 
+import java.time.LocalDateTime;
+
 public class Forecast {
 
 	private double temperature;
 	private City city;
+	private LocalDateTime timeStamp;
+	
+	public Forecast(){
+		
+	}
+	
+	public Forecast(double temperature, City city, LocalDateTime timeStamp){
+		this.setCity(city);
+		this.setTemperature(temperature);
+		this.setTimeStamp(timeStamp);
+	}
+
+	public double getTemperature() {
+		return temperature;
+	}
+
+	public void setTemperature(double temperature) {
+		this.temperature = temperature;
+	}
+
+	public City getCity() {
+		return city;
+	}
+
+	public void setCity(City city) {
+		this.city = city;
+	}
+
+	public LocalDateTime getTimeStamp() {
+		return timeStamp;
+	}
+
+	public void setTimeStamp(LocalDateTime timeStamp) {
+		this.timeStamp = timeStamp;
+	}
+	
+	
 	
 }
