fbd86311767f27462e944adfc4f8e0ab50d3fef2
Tim <tim.debruyne@hotmail.com>
2016-09-22 14:59:29 +0200

    added DayForecast and WeekForecast

---
+++ WeatherApp-App/WeatherApp-Domain/src/main/java/model/DayForecast.java
@@ -0,0 +1,6 @@
+package model;
+
+public class DayForecast {
+
+	private double temp;
+}
---
+++ WeatherApp-App/WeatherApp-Domain/src/main/java/model/WeekForecast.java
@@ -0,0 +1,10 @@
+package model;
+
+import java.util.ArrayList;
+
+public class WeekForecast {
+
+	private String country;
+	private String city;
+	private ArrayList<DayForecast> forecasts;
+}
