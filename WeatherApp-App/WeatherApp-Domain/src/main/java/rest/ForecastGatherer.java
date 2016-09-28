a46f38afe03a06399d17cac909ae15678ddc8711
Tim <tim.debruyne@hotmail.com>
2016-09-28 19:45:16 +0200

    Created First version of REST Program + dependencies

--- WeatherApp-App/WeatherApp-Domain/src/main/java/rest/ForecastGatherer.java
+++ WeatherApp-App/WeatherApp-Domain/src/main/java/rest/ForecastGatherer.java
@@ -1,5 +1,23 @@
 package rest;
+import javax.ws.rs.*;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.json.JsonArray;
+
 
 public class ForecastGatherer {
-
+	
+	
+	
+	Client client=ClientBuilder.newClient();
+	//TODO ADD REST URL
+	WebTarget target = client.target("enterurlhere");
+		//.queryParam("paramname","value");
+	JsonArray response=target.request(MediaType.APPLICATION_JSON)
+			.get(JsonArray.class);
+	// do stuff with results after this
+	
 }
