037e6c5cf55ce3a94a73887d7e6802782d0be030
Sander Joos <sander.joos@skynet.be>
2016-10-13 14:05:16 +0200

    tim fucked up

--- WeatherApp-App/WeatherApp-Domain/src/main/java/rest/ForecastGatherer.java
+++ WeatherApp-App/WeatherApp-Domain/src/main/java/rest/ForecastGatherer.java
@@ -5,19 +5,24 @@ import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Invocation;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
+
+import model.City;
+
 import javax.json.JsonArray;
 
 
 public class ForecastGatherer {
-	
-	private final String URL = "";
-	
-	Client client=ClientBuilder.newClient();
-	//TODO ADD REST URL
-	WebTarget target = client.target("enterurlhere");
-		//.queryParam("paramname","value");
-	JsonArray response=target.request(MediaType.APPLICATION_JSON)
-			.get(JsonArray.class);
-	// do stuff with results after this
-	
+
+    private final String URL = "http://api.wunderground.com/api/b2d97c37f90887e5/conditions/q/";
+    public City getCityForecast(){
+
+    Client client=ClientBuilder.newClient();
+    WebTarget target = client.target(URL+"BE"+"/"+"Leuven"+".jsp");
+    JsonArray response=target.request(MediaType.APPLICATION_JSON)
+            .get(JsonArray.class);
+    // do stuff with results after this
+    City cityresp=new City("Belgium","Leuven");
+    System.out.println(response);
+    return cityresp;
+    }
 }
