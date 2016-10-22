181b2e543f5dc70f93773b07909632b4e4513938
Sander Joos <sander.joos@skynet.be>
2016-10-22 17:38:53 +0200

    made rest

---
+++ WeatherApp-App/WeatherApp-Rest/src/main/java/weatherapp/weatherapp/rest/WeatherResource.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package weatherapp.weatherapp.rest;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.MediaType;
+import service.WeatherService;
+
+/**
+ * REST Web Service
+ *
+ * @author Sander_2
+ */
+@Path("/weather")
+public class WeatherResource {
+    
+    private WeatherService service;
+
+    /**
+     * Creates a new instance of WeatherResource
+     */
+    public WeatherResource() {
+        this.service = new WeatherService();
+    }
+
+    @GET
+    @Produces("text/plain")
+    public String getMessage(){
+        return "hallo";
+    }
+    
+    @Path("{country}-{city}")
+    @GET
+    @Produces("text/plain")
+    public String getForecastForCity(@PathParam("country")String country, @PathParam("city")String city) {
+        return this.service.getForecastForCityStringsAsString(city, country);
+    }
+}
